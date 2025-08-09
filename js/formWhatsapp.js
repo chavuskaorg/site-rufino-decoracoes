3d9d3eb865890dbcf7528b737180905b5f810cc3
Chavuks <16940496+Chavuks@users.noreply.github.com>
2025-08-09 01:37:29 -0300

    commit pages and tailwindcss

---
+++ js/formWhatsapp.js
@@ -0,0 +1,13 @@
+
+function enviarParaWhatsapp() {
+  alert("Mensagem enviada com sucesso!"); // Exibe o alerta de sucesso
+  const nome = document.getElementById("nome").value;
+  const mensagem = document.getElementById("mensagem").value;
+
+  const texto = `Olá! Meu nome é ${nome}. ${mensagem}`;
+  const numero = "5511949768699"; // Substitua pelo seu número (com DDD e sem espaços)
+
+  const url = `https://wa.me/${numero}?text=${encodeURIComponent(texto)}`;
+  window.open(url, '_blank');
+}
+    
